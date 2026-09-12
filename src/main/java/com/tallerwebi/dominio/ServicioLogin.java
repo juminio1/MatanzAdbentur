57b87e424d8b20ccb41dd47795cc2fd4af077850
Rafael Possentini <rafa08poss06@gmail.com>
2026-09-12 14:13:09 -0300

    Se modifica el login implementando la autenticación con hash y el controlador de manejo de inicio sesión

--- src/main/java/com/tallerwebi/dominio/ServicioLogin.java
+++ src/main/java/com/tallerwebi/dominio/ServicioLogin.java
@@ -1,6 +1,8 @@
 package com.tallerwebi.dominio;
 
-@SuppressWarnings("PMD.ImplicitFunctionalInterface")
+import com.tallerwebi.dominio.excepcion.CredencialesInvalidasException;
+
 public interface ServicioLogin {
   Usuario consultarUsuario(String email);
+  Usuario autenticar(String email, String password) throws CredencialesInvalidasException; // o DatosInvalidosException
 }
