b0f4582fad42d5baad847949d2b2e2be381ed010
Rafael Possentini <rafa08poss06@gmail.com>
2026-09-22 20:08:23 -0300

    Se agrega el enum EstadoPartida y se implenta al servicio

--- src/main/java/com/tallerwebi/dominio/Partida.java
+++ src/main/java/com/tallerwebi/dominio/Partida.java
@@ -11,13 +11,28 @@ public class Partida {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   private String codigoUnico;
-  private Usuario Creador;
+
   private Instant tiempoInicio;
+
+  @ManyToOne
+  private Usuario creador;
+
+  @Enumerated(EnumType.STRING)
+  private EstadoPartida estado;
+
   @OneToOne(cascade = CascadeType.PERSIST)
   private Tablero tablero;
+
   @ManyToMany
   private List<Usuario> usuarios;
+
+  public Partida() {
+    this.usuarios = new ArrayList<>();
+    this.estado = EstadoPartida.EN_ESPERA; // Nace en sala de espera / lobby
+  }
+
   public Long getId() {
     return id;
   }
@@ -26,12 +41,44 @@ public class Partida {
     this.id = id;
   }
 
+  public String getCodigoUnico() {
+    return codigoUnico;
+  }
+
+  public void setCodigoUnico(String codigoUnico) {
+    this.codigoUnico = codigoUnico;
+  }
+
   public Instant getTiempoInicio() {
     return tiempoInicio;
   }
 
+  public void setTiempoInicio(Instant tiempoInicio) {
+    this.tiempoInicio = tiempoInicio;
+  }
+
+  public Usuario getCreador() {
+    return creador;
+  }
+
+  public void setCreador(Usuario creador) {
+    this.creador = creador;
+  }
+
+  public EstadoPartida getEstado() {
+    return estado;
+  }
+
+  public void setEstado(EstadoPartida estado) {
+    this.estado = estado;
+  }
+
   public Tablero getTablero() {
     return tablero;
+  }
+
+  public void setTablero(Tablero tablero) {
+    this.tablero = tablero;
   }
 
   public List<Usuario> getUsuarios() {
@@ -42,54 +89,18 @@ public class Partida {
     this.usuarios = usuarios;
   }
 
-  public Boolean getActiva() {
-    return activa;
+  public Boolean agregarUsuario(Usuario usuario) {
+    if (this.usuarios.size() < 4 && this.estado == EstadoPartida.EN_ESPERA) {
+      this.usuarios.add(usuario);
+      if (this.usuarios.size() == 4) {
+        this.estado = EstadoPartida.EN_CURSO; // Se llenó el cupo: arranca el juego
+      }
+      return true;
+    }
+    return false;
   }
 
-  public void setActiva(Boolean activa) {
-    this.activa = activa;
+  public void finalizar() {
+    this.estado = EstadoPartida.FINALIZADA;
   }
-
-  private Boolean activa;
-
-  public Partida(){
-        this.usuarios = new ArrayList<>();
-       this.activa = true;
-    }
-
-  public Boolean agregarUsuario(Usuario usuario){
-        if (this.usuarios.size() < 4){
-            this.usuarios.add(usuario);
-            return true;
-        }
-        return false;
-    }
-
-  public void setTablero(Tablero tablero) {
-    this.tablero = tablero;
-  }
-
-  public void setTiempoInicio(Instant tiempoInicio) {
-        this.tiempoInicio = tiempoInicio;
-    }
-
-  public void finalizar(){
-        this.activa = false;
-    }
-
-    public String getCodigoUnico() {
-        return codigoUnico;
-    }
-
-    public void setCodigoUnico(String codigoUnico) {
-        this.codigoUnico = codigoUnico;
-    }
-
-    public Usuario getCreador() {
-        return Creador;
-    }
-
-    public void setCreador(Usuario creador) {
-        Creador = creador;
-    }
 }
